7f92a4e1e2c750217aa84db2a1ca49b88ec094cf
Adi-12 <adiiskandarubaidah@gmail.com>
2022-12-25 18:07:20 +0700

    Fix notif contact

--- method.js
+++ method.js
@@ -1,10 +1,6 @@
-const test = $(".personal-skills").offset().top -500;
+
 $(window).scroll(function(){
 	$("nav").toggleClass("window-scroll", $(window).scrollTop() > 0);
-	if ($(window).scrollTop() > test) {	
-		gsap.to(".progress-in", {x:0,duration:2,ease:'slow'});
-
-	}
 	// console.log($(window).scrollTop());
 });
 
@@ -47,7 +43,7 @@ const closeNav = () => {
 closeBtn.addEventListener("click", closeNav);
 
 
-
+//animasi scroll
 $(".page-scroll").on("click", function(e) {
 	//ambil atribut href
 	const tujuan = $(this).attr('href');
@@ -60,37 +56,56 @@ $(".page-scroll").on("click", function(e) {
 	});
 
 });
+//pengiriman pesan
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzrERQ1SRgGYMAO_V9wAmbtfm8teYwQFWKH84CQaDnvGQxdJ1u9sqEhpl0l1wm-S0X0/exec';
   const form = document.forms['adi-contact-form'];
-  const notif = document.querySelector(".notif");
-  const notifClose = document.querySelector(".notif > span");
+  const notif= document.querySelectorAll(".notif");
+  const notifClose = document.querySelectorAll(".notif > span");
   const kirim = document.querySelector("button[type=submit] p");
   const loader = document.querySelector("button[type=submit] .loader");
-
+  
   form.addEventListener('submit', e => {
     e.preventDefault();
     //text kirim dihapus
     kirim.classList.toggle('d-none');
     //loader dimunculkan
     loader.classList.toggle('d-none');
-    if (!notif.classList.contains('d-none')) notif.classList.add('d-none');
+
+    notif.forEach(function(e) {
+    	if (!e.classList.contains('d-none')) e.classList.add('d-none');
+    });
 
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
       .then(response => {
       	//kirim dimunculkan
       	kirim.classList.toggle('d-none');
       	//loader dihapus
-    	loader.classList.toggle('d-none');
-    	notif.classList.toggle('d-none');
+      	loader.classList.toggle('d-none');
+    	notif[0].classList.toggle('d-none');
 
     	//form direset
       	form.reset();
       	console.log('Success!', response);
-  })
-      .catch(error => console.error('Error!', error.message))
+      	notifClose[0].addEventListener("click", function() {
+		notif[0].classList.add("d-none");
+		});
+
+  	})
+      .catch(error => {
+      
+      	notif[1].classList.toggle('d-none');
+      	kirim.classList.toggle('d-none');
+      	loader.classList.toggle('d-none');
+
+      	console.error('Error!', error.message)
+		//tutup notifkasinya
+		notifClose[1].addEventListener("click", function() {
+		notif[1].classList.add("d-none");
+		});
+
+   	});
+     
+
   });
-//tutup notifkasinya
-  notifClose.addEventListener("click", function() {
-  	notif.classList.add("d-none");
-  });
+
